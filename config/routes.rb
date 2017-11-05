5d15a0a35f2433a07695f96688d1d04247414b31
Minh Nguyen <mhnguyen289@gmail.com>
2017-11-04 17:34:53 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: JSON} do
-  	
-  	resources :order, only: [:index, :new, :create, :update,:edit,:delete] do
+  		resources :order, only: [:index, :new, :create, :update,:edit,:delete] do
+  			
   		  	resources :item, only: [:index, :create, :update,:edit,:delete]
-  		
-  		get 'orders/phone_number', to: 'orders/:id'
-
+  		end
+  		get 'orders/phone_number', to: 'orders#:id'
   	end
-  end
-end
+
 end
